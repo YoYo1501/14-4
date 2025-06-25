bf6b83ea85beb56d9a83d0acadf7911f078290e0
Quang Sanh <buiquangsanh1501@gmail.com>
2025-06-25 19:29:49 +0700

    Update: add Dockerfile, docker-compose.yml and update main.js

--- main.js
+++ main.js
@@ -1,36 +1,43 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import router from './src/routes/index.js'
-import mongoInstance from './src/configs/mongoose.config.js'
-const app = express()
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import router from "./src/routes/index.js";
+import mongoInstance from "./src/configs/mongoose.config.js";
+import cookieParser from "cookie-parser";
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
 
 async function startServer() {
-  const connectString = process.env.MONGODB_URI
-  console.log('Connecting to MongoDB...')
-  await mongoInstance.connect(connectString)
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: true }))
-  app.use(express.static(path.join(__dirname, 'public')))
-  app.use('/api', router)
+  const connectString = process.env.MONGODB_URI;
+  console.log("Connecting to MongoDB...");
+  await mongoInstance.connect(connectString);
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, "public")));
+  app.use("/uploads", express.static(path.join(__dirname, "common/uploads")));
+  app.use(cookieParser());
+  app.use("/api", router);
+  app.get("/", (req, res) => {
+    res.send("Welcome to DemoExpress app 🚀");
+  });
+
   app.use((req, res) => {
-    res.status(404).send('Not Found')
-  })
+    res.status(404).send("Not Found");
+  });
   app.use((err, req, res) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-  })
-  const PORT = process.env.PORT || 3000
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+  });
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-  })
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
 try {
-    await startServer()
+  await startServer();
 } catch (error) {
-    console.error('Error starting server:', error)
+  console.error("Error starting server:", error);
 }
